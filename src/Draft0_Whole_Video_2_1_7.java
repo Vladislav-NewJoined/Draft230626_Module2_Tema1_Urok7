8912e1837545271f9b3df01afe7e3fab8b687fb4
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-26 13:01:29 +0300

    Initial commit

--- src/Draft0_Whole_Video_2_1_7.java
+++ src/Draft0_Whole_Video_2_1_7.java
@@ -1,5 +1,7 @@
-public class Main {
+public class Draft0_Whole_Video_2_1_7 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание : \n1.\tИспользуя Stream вам необходимо объединить данные три строки " +
+                "\n“I”, “LOVE”,“JAVA” вы создаете новый поток из простых строк, а затем собираете их в " +
+                "\nодну при помощи метода collect()\n\nРешение: ");
     }
 }
